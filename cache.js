7d80c3d1d846bc80ec0e43817706bd08f9bcd21d
Akhil Ravindran <imakhilravindran@gmail.com>
2022-06-26 17:13:54 +0530

    feat: docker and docker compose added

--- cache.js
+++ cache.js
@@ -5,7 +5,9 @@ class Cache {
 
     // create client and check for any connection error
     constructor(){
-        this.client = createClient()
+        const redisURL = process.env.REDIS_URL || 'redis://localhost:6379'
+        console.log(redisURL);
+        this.client = createClient({url: redisURL})
         this.client.on('err', (err) => console.log('Redis client error: ', err))
     }
 
